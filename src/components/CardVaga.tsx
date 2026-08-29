649e4534af4c09d2e793e78b482f43ee631443c2
marcosrabelocod <marcosrabelocod@gmail.com>
2026-08-29 00:18:40 -0300

    fix: atualização do tipo e do card de vaga para agregar data e regime

--- src/components/CardVaga.tsx
+++ src/components/CardVaga.tsx
@@ -12,11 +12,13 @@ export default function CardVaga({vaga}: CardVagasProps){
             <h3 className="font-bold text-lg text-slate-800">{vaga.titulo}</h3>
             <p className="text-sm text-slate-500 mb-2">{vaga.empresa}</p>
 
+            <div className="flex flex-wrap gap-2 mb-3">
+                <span className="inline-block bg-blue-100 text-blue-700 text-xs px-2 py-1 rounded font-medium">
+                    {vaga.area}
+                </span>
 
-            <span className="inline-block bg-blue-100 text-blue-700 text-xs px-2 py-1 rounded font-medium mb-3">
-                {vaga.area}
-            </span>
-
+                <span className="inline-block bg-purple-100 text-purple-700 text-xs px-2 py-1 rounded font-medium">{vaga.regime}</span>
+            </div>
             {/*sistema de tags preparado para receber eventos de click e filtro*/}
             <div className="flex flex-wrap gap-2 mb-4">
                 {vaga.tecnologias.map((tag, index) => (
@@ -26,11 +28,15 @@ export default function CardVaga({vaga}: CardVagasProps){
                 ))}
             </div>
 
-            <a href={vaga.link} 
-            className="text-sm font-semibold text-blue-600 hover:underline" 
-            target="_blank">
-                Ver Vaga
-            </a>
+            <div className="flex item-center justify-between pt-3 mt-1 border-t border-slate-100">
+                <a href={vaga.link} 
+                className="text-sm font-semibold text-blue-600 hover:underline" 
+                target="_blank">
+                    Ver Vaga
+                </a>
+
+                <span className="text-xs font-medium text-slate-400">{vaga.data}</span>
+            </div>
         </div>
     )
 }
--- src/type/Vaga.ts
+++ src/type/Vaga.ts
@@ -5,7 +5,10 @@ export interface Vaga {
     area: string;
     descricao: string;
     link: string;
+    //no campo tecnologias colocamos linguagens frameworks bibliotecas e ferramentas que a vaga possa exigir
     tecnologias: string[]
+    data: string
+    regime: 'homeoffice' | 'hibrido' | 'presencial'
 }
 
 export const MockVagas: Vaga[] =[
@@ -16,6 +19,8 @@ export const MockVagas: Vaga[] =[
     area: 'Next/TypeScript',
     descricao: 'Atuar na manutenção de um dashboard SaaS...',
     link: 'https://linkedin.com/vaga1',
-    tecnologias: ['Next', 'React', 'Tailwind'] 
+    tecnologias: ['Next', 'React', 'Tailwind', 'mvc', 'gitlab'],
+    data: '23/08/2026',
+    regime: 'homeoffice'
   },
 ]
